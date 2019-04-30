454560a775e3bb3759c6c1c9ff349181c3942345
ABrambleNinja <abrambleninja@riseup.net>
2019-04-30 05:41:52 -0700

    Implement parallel camera

--- raytracer/cameras/Parallel.cpp
+++ raytracer/cameras/Parallel.cpp
@@ -1,16 +1,22 @@
 #include "Parallel.hpp"
 
-Parallel::Parallel() {}
-
-Parallel::Parallel(float c) {}
-
-Parallel::Parallel(float x, float y, float z) {}
-
-Parallel::Parallel(const Vector3D& d) {}
-
-Parallel* Parallel::clone() const { return nullptr; }
-
-Vector3D Parallel::get_direction(const Point3D& p) const {
-  Vector3D temp;
-  return temp;
+Parallel::Parallel() : dir{Vector3D(0, 0, -1)} {
+  // nothing else to do
 }
+
+Parallel::Parallel(float c) : dir{Vector3D(c).normalize()} {
+  // nothing else to do
+}
+
+Parallel::Parallel(float x, float y, float z)
+    : dir{Vector3D{x, y, z}.normalize()} {
+  // nothing else to do
+}
+
+Parallel::Parallel(const Vector3D& d) : dir{Vector3D(d).normalize()} {
+  // nothing else to do
+}
+
+Parallel* Parallel::clone() const { return new Parallel(*this); }
+
+Vector3D Parallel::get_direction(const Point3D& p) const { return dir; }
