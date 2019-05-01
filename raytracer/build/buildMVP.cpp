1613ef8a4f994f30d4cd28f6c8c53aefafcceb99
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2019-05-01 02:58:47 -0700

    Cleaning up for the night

---
+++ raytracer/build/buildMVP.cpp
@@ -0,0 +1,35 @@
+#include "../cameras/Parallel.hpp"
+#include "../geometry/Plane.hpp"
+#include "../geometry/Sphere.hpp"
+#include "../geometry/Triangle.hpp"
+#include "../materials/Cosine.hpp"
+#include "../samplers/Simple.hpp"
+#include "../utilities/Constants.hpp"
+#include "../world/World.hpp"
+
+void World::build(void) {
+  // View plane  .
+  vplane.top_left.x = -10;
+  vplane.top_left.y = 10;
+  vplane.top_left.z = 10;
+  vplane.bottom_right.x = 10;
+  vplane.bottom_right.y = -10;
+  vplane.bottom_right.z = 10;
+  vplane.hres = 400;
+  vplane.vres = 400;
+
+  // Background color.
+  bg_color = black;
+
+  // Camera and sampler.
+  set_camera(new Parallel(0, 0, -1));
+  sampler_ptr = new Simple(camera_ptr, &vplane);
+
+  for (int x = -8; x <= 8; x += 2) {
+    for (int y = -8; y <= 8; y += 2) {
+      Sphere* s = new Sphere(Point3D(x, y, 0), 1);
+      s->set_material(new Cosine((x + 8.0) / 16.0, 0, (y + 8.0) / 16.0));
+      add_geometry(s);
+    }
+  }
+}
