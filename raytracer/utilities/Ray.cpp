63c11402f120352771d7318ed8da612b9b5c8501
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2019-05-06 10:36:29 -0700

    Added constructor for specifying Ray weight

--- raytracer/utilities/Ray.cpp
+++ raytracer/utilities/Ray.cpp
@@ -4,3 +4,6 @@ Ray::Ray() : o{0}, d{0}, w{1} {}
 
 Ray::Ray(const Point3D& origin, const Vector3D& dir)
     : o{origin}, d{dir}, w{1} {}
+
+Ray::Ray(const Point3D& origin, const Vector3D& dir, double weight)
+    : o{origin}, d{dir}, w{weight} {}
--- raytracer/utilities/Ray.hpp
+++ raytracer/utilities/Ray.hpp
@@ -14,12 +14,13 @@ class Ray {
  public:
   Point3D o;   // origin
   Vector3D d;  // direction, keep normalized.
-  float w;     // weightage of ray for a particular pixel, default is 1.
+  double w;     // weightage of ray for a particular pixel, default is 1.
 
  public:
   // Constructors.
   Ray();  // set origin and dir to (0, 0, 0), w to 1.
   Ray(const Point3D& origin, const Vector3D& dir);  // set origin, dir; w is 1.
+  Ray(const Point3D& origin, const Vector3D& dir, double weight);
 
   // Copy constructor and assignment operator.
   Ray(const Ray& ray) = default;
