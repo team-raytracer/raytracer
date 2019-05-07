b8d1f01676449376f8e6b62e4c8466cddc8ac2e8
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2019-05-07 02:05:46 -0700

    Generate chess board from input string

--- raytracer/build/buildChess.cpp
+++ raytracer/build/buildChess.cpp
@@ -1,3 +1,6 @@
+#include <istream>
+#include <map>
+#include <sstream>
 #include "../cameras/Perspective.hpp"
 #include "../geometry/Plane.hpp"
 #include "../geometry/Sphere.hpp"
@@ -8,34 +11,74 @@
 #include "../utilities/Vector3D.hpp"
 #include "../world/World.hpp"
 
-enum class ChessPieces { Pawn, Knight, Bishop, Rook, Queen, King };
+const RGBColor TEAM_COLORS[2] = {blue, red};
+const double PIECE_SIZE_OFFSET = 0.1;
+const std::string DEFAULT_BOARD =
+    "r1 h1 b1 q1 k1 b1 h1 r1 \n"
+    "p1 p1 p1 p1 p1 p1 p1 p1 \n"
+    "xx xx xx xx xx xx xx xx \n"
+    "xx xx xx xx xx xx xx xx \n"
+    "xx xx xx xx xx xx xx xx \n"
+    "xx xx xx xx xx xx xx xx \n"
+    "p0 p0 p0 p0 p0 p0 p0 p0 \n"
+    "r0 h0 b0 k0 q0 b0 h0 r0 \n";
 
-void addPiece(World* world, std::string pieceName, RGBColor color, int x,
-              int y) {
-  world->add_ply("models/" + pieceName + ".ply", new Cosine(color),
-                 Point3D(x + 0.1, y + 0.1, 0), Point3D(x + 0.9, y + 0.9, 2));
+void addPiece(World* world, std::string pieceName, size_t team, size_t x,
+              size_t y) {
+  world->add_ply(
+      "models/" + pieceName + ".ply", new Cosine(TEAM_COLORS[team]),
+      Point3D(x + PIECE_SIZE_OFFSET, y + PIECE_SIZE_OFFSET, 0),
+      Point3D(x + 1 - PIECE_SIZE_OFFSET, y + 1 - PIECE_SIZE_OFFSET, 2), true);
+}
+
+void parseBoard(World* world, std::istream& board,
+                std::map<char, std::string>& dict) {
+  std::string piece;
+  for (size_t x = 0; x < 8; ++x) {
+    for (size_t y = 0; y < 8; ++y) {
+      std::getline(board, piece, ' ');
+      if (piece.length() == 2 && dict.count(piece[0]) > 0 &&
+          (piece[1] == '0' || piece[1] == '1')) {
+        addPiece(world, dict[piece[0]], piece[1] - '0', x, y);
+      }
+    }
+  }
+}
+
+std::map<char, std::string> initializeDictionary() {
+  std::map<char, std::string> dict;
+  dict['p'] = "pawn";
+  dict['h'] = "knight";
+  dict['b'] = "bishop";
+  dict['r'] = "rook";
+  dict['q'] = "queen";
+  dict['k'] = "king";
+
+  return dict;
 }
 
 void World::build(void) {
+  std::map<char, std::string> dict = initializeDictionary();
+
   // view plane
-  vplane.top_left.x = -4;
-  vplane.top_left.y = 4;
+  vplane.top_left.x = 1;
+  vplane.top_left.y = 7;
   vplane.top_left.z = 5;
-  vplane.bottom_right.x = 4;
-  vplane.bottom_right.y = -4;
+  vplane.bottom_right.x = 7;
+  vplane.bottom_right.y = 1;
   vplane.bottom_right.z = 5;
-  vplane.hres = 400;
-  vplane.vres = 400;
+  vplane.hres = 200;
+  vplane.vres = 200;
 
   bg_color = gray;  // background color.
 
   // camera and sampler.
-  set_camera(new Perspective(0, 0, 10));
+  set_camera(new Perspective(4, 4, 10));
   sampler_ptr = new Simple(camera_ptr, &vplane);
 
   // Generate chess board
-  for (int y = -4; y < 4; ++y) {
-    for (int x = -4; x < 4; ++x) {
+  for (size_t y = 0; y < 8; ++y) {
+    for (size_t x = 0; x < 8; ++x) {
       RGBColor color = (x + y) % 2 == 0 ? white : black;
 
       Triangle* triangle = new Triangle(Point3D(x, y, 0), Point3D(x + 1, y, 0),
@@ -50,8 +93,7 @@ void World::build(void) {
     }
   }
 
-  addPiece(this, "queen", blue, -4, -4);
-
-  // add_ply("models/queen.ply", new Cosine(blue), Point3D(-4, -4, 0),
-  //         Point3D(-3, -3, 2));
+  std::stringstream stream;
+  stream << DEFAULT_BOARD;
+  parseBoard(this, stream, dict);
 }
