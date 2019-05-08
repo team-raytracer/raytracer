cf609be1a1513ffd655f41956901a99310bc126f
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2019-05-08 10:43:40 -0700

    Finessing chess scene, lower poly models

--- raytracer/build/buildChess.cpp
+++ raytracer/build/buildChess.cpp
@@ -6,8 +6,11 @@
 #include "../geometry/Plane.hpp"
 #include "../geometry/Sphere.hpp"
 #include "../geometry/Triangle.hpp"
+#include "../lights/Ambient.hpp"
+#include "../lights/Point.hpp"
 #include "../materials/Cosine.hpp"
-#include "../samplers/Simple.hpp"
+#include "../materials/Matte.hpp"
+#include "../samplers/RegularBox.hpp"
 #include "../tracers/Whitted.hpp"
 #include "../utilities/Constants.hpp"
 #include "../utilities/Vector3D.hpp"
@@ -16,10 +19,12 @@
 #include <iostream>
 #include "../cameras/Parallel.hpp"
 
-const RGBColor TEAM_COLORS[2] = {blue, red};
+const RGBColor TEAM_COLORS[2] = {RGBColor(0.3, 0.56, 0.64), RGBColor(1, 0.56, 0.52)};
 const Point3D CAMERA_POSITION = Point3D(9, 4, -3.25);
-const size_t RESOLUTION = 200;
+const size_t RESOLUTION = 1080;
 const double PIECE_SIZE_OFFSET = 0.1;
+const double KA = 0.2;
+const double KD = 3;
 const char CHESS_FILE_NAME[16] = "chessLayout.txt";
 const char DEFAULT_BOARD[193] =
     "r1 h1 b1 q1 k1 b1 h1 r1\n"
@@ -33,8 +38,12 @@ const char DEFAULT_BOARD[193] =
 
 void addPiece(World* world, std::string pieceName, size_t team, size_t x,
               size_t z) {
+  Matte* matte = new Matte();
+  matte->set_kd(KD);
+  matte->set_ka(KA);
+  matte->set_cd(TEAM_COLORS[team]);    
   world->add_ply(
-      "models/" + pieceName + ".ply", new Cosine(TEAM_COLORS[team]),
+      "models/" + pieceName + ".ply", matte,
       Point3D(x + PIECE_SIZE_OFFSET, 0, z + PIECE_SIZE_OFFSET),
       Point3D(x + 1 - PIECE_SIZE_OFFSET, 2, z + 1 - PIECE_SIZE_OFFSET), true);
 }
@@ -59,12 +68,12 @@ void parseBoard(World* world, std::istream& board,
 
 std::map<char, std::string> initializeDictionary() {
   std::map<char, std::string> dict;
-  dict['p'] = "bun_zipper_res4";  // "pawn";
-  dict['h'] = "bun_zipper_res4";  // "knight";
-  dict['b'] = "bun_zipper_res4";  // "bishop";
-  dict['r'] = "bun_zipper_res4";  // "rook";
-  dict['q'] = "bun_zipper_res4";  // "queen";
-  dict['k'] = "bun_zipper_res4";  // "king";
+  dict['p'] = "pawn";
+  dict['h'] = "knight";
+  dict['b'] = "bishop";
+  dict['r'] = "rook";
+  dict['q'] = "queen";
+  dict['k'] = "king";
 
   return dict;
 }
@@ -81,24 +90,47 @@ void World::build(void) {
 
   // camera and sampler.
   set_camera(new Perspective(CAMERA_POSITION));
-  sampler_ptr = new Simple(camera_ptr, &vplane);
+  sampler_ptr = new RegularBox(camera_ptr, &vplane, 2);
 
   // Tracer
   tracer_ptr = new Whitted(&(*this));
 
+  // Materials
+  Matte* matte = new Matte();
+  matte->set_kd(KD);
+  matte->set_ka(KA);
+
+  // Lights
+  set_ambient_light(new Ambient());
+  Point* point = new Point();
+  point->set_position(1, 4, 7);
+  add_light(point);
+  
+  point = new Point(1, 0, 0);
+  point->set_position(7, 4, 7);
+  add_light(point);
+
+  point = new Point(0, 0, 1);
+  point->set_position(1, 4, 1);
+  add_light(point);
+
+  point = new Point(0.2);
+  point->set_position(9, 4, -1);
+  add_light(point);
+
   // Generate chess board
   for (size_t z = 0; z < 8; ++z) {
     for (size_t x = 0; x < 8; ++x) {
-      RGBColor color = (x + z) % 2 == 0 ? white : black;
+      matte->set_cd((x + z) % 2 == 0 ? white : black);
 
       Triangle* triangle = new Triangle(Point3D(x, 0, z), Point3D(x, 0, z + 1),
                                         Point3D(x + 1, 0, z));
-      triangle->set_material(new Cosine(color));
+      triangle->set_material(matte->clone());
       add_geometry(triangle);
 
       triangle = new Triangle(Point3D(x + 1, 0, z + 1), Point3D(x + 1, 0, z),
                               Point3D(x, 0, z + 1));
-      triangle->set_material(new Cosine(color));
+      triangle->set_material(matte->clone());
       add_geometry(triangle);
     }
   }
@@ -111,4 +143,6 @@ void World::build(void) {
     stream << DEFAULT_BOARD;
     parseBoard(this, stream, dict);
   }
+
+  delete matte;
 }
