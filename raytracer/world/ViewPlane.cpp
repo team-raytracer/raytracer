ced40c45f376d3ccc5e85442994893981d72b89b
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2019-05-08 02:46:29 -0700

    Add parameterized constructors for viewplane

--- raytracer/world/ViewPlane.cpp
+++ raytracer/world/ViewPlane.cpp
@@ -7,6 +7,18 @@ ViewPlane::ViewPlane()
       hres{640},
       vres{480} {}
 
+ViewPlane::ViewPlane(Point3D top_left, Point3D bottom_right, size_t hres,
+                     size_t vres)
+    : top_left{top_left}, bottom_right{bottom_right}, hres{hres}, vres{vres} {}
+
+ViewPlane::ViewPlane(Point3D top_left, Point3D bottom_right,
+                     Point3D top_left_back, size_t hres, size_t vres)
+    : top_left{top_left},
+      bottom_right{bottom_right},
+      top_left_back{top_left_back},
+      hres{hres},
+      vres{vres} {}
+
 Point3D ViewPlane::getPixelPoint(double px, double py) const {
   Vector3D dif = bottom_right - top_left;
   return top_left + Vector3D(px / hres * dif.x, py / vres * dif.y, 0);
--- raytracer/world/ViewPlane.hpp
+++ raytracer/world/ViewPlane.hpp
@@ -15,15 +15,19 @@
 
 class ViewPlane {
  public:
-  Point3D top_left;      // top left corner of the view plane.
-  Point3D bottom_right;  // bottom right corner of the view plane.
-  size_t hres;           // horizontal resolution
-  size_t vres;           // vertical resolution
+  Point3D top_left;       // top left corner of the view plane.
+  Point3D bottom_right;   // bottom right corner of the view plane.
+  Point3D top_left_back;  // Option third point for viewplanes with freed z
+  size_t hres;            // horizontal resolution
+  size_t vres;            // vertical resolution
 
  public:
-  // Constructor
+  // Constructors
   ViewPlane();  // default: 640 x 480 view plane at (-320, -240) with pixel
                 // size 1.
+  ViewPlane(Point3D top_left, Point3D bottom_right, size_t hres, size_t vres);
+  ViewPlane(Point3D top_left, Point3D bottom_right, Point3D top_left_back,
+            size_t hres, size_t vres);
 
   // Copy constructor and assignment operator.
   ViewPlane(const ViewPlane& other) = default;
