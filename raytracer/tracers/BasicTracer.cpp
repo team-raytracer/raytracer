9441df94cacb9c6755ab7c5794c70f7d4ca52ad4
davidfsobek <davidfsobek@gmail.com>
2019-05-08 16:43:30 -0700

    made basic ray tracer only use primary rays instead of infinite secondary

--- raytracer/tracers/BasicTracer.cpp
+++ raytracer/tracers/BasicTracer.cpp
@@ -11,10 +11,16 @@ BasicTracer::BasicTracer(World* _worldPtr) : Tracer(_worldPtr) {}
 BasicTracer::~BasicTracer() {}
 
 RGBColor BasicTracer::trace_ray(const Ray ray, const int depth) const {
-  ShadeInfo sr(world_ptr->hit_objects(ray));
-  if (sr.hit) {
-    sr.ray = ray;
-    return ray.w * (sr.material_ptr->shade(sr));
-  } else
-    return (ray.w * world_ptr->bg_color);
+  if (depth > 0)
+    return (black);
+  else {
+    ShadeInfo sr(world_ptr->hit_objects(ray));
+
+    if (sr.hit) {
+      sr.depth = depth;
+      sr.ray = ray;
+      return ray.w * (sr.material_ptr->shade(sr));
+    } else
+      return (ray.w * world_ptr->bg_color);
+  }
 }
